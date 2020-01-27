72126e24dc899e75bf867e5114ff398dc557b9e1
Sofiyullah jamiu <sofiyullahj@gmail.com>
2020-01-27 19:40:54 +0100

    fixed image size

--- server.js
+++ server.js
@@ -44,8 +44,9 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/.", function(req, res) {
   return res.send("pong");
-});
+});ap
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(port, () => console.log(`listening on port ${port}`));
+a
